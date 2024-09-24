8c10b24716e69eefb401142f886435bfb1674625
AHFAH <jiyou626@gmail.com>
2024-09-24 20:41:56 +0900

    [ADD] 도서 예약 API

---
+++ src/main/java/com/likelion/lionlib/service/ReservationService.java
@@ -0,0 +1,67 @@
+package com.likelion.lionlib.service;
+
+import com.likelion.lionlib.domain.Book;
+import com.likelion.lionlib.domain.Member;
+import com.likelion.lionlib.domain.Reservation;
+import com.likelion.lionlib.dto.request.ReservationRequest;
+import com.likelion.lionlib.dto.response.ReservationCountResponse;
+import com.likelion.lionlib.dto.response.ReservationResponse;
+import com.likelion.lionlib.repository.ReservationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ReservationService {
+    private final ReservationRepository repository;
+    private final GlobalService globalService;
+
+    // 도서 예약
+    public ReservationResponse addReservation(ReservationRequest request) {
+        Member member = globalService.findMemberById(request.getMemberId());
+        Book book = globalService.findBookById(request.getBookId());
+
+        Reservation savedReservation = Reservation.builder()
+                .member(member)
+                .book(book)
+                .build();
+        repository.save(savedReservation);
+        return ReservationResponse.fromEntity(savedReservation);
+    }
+
+    // 예약 조회
+    public ReservationResponse getReservation(Long reservationId) {
+        Reservation reservation = repository.findById(reservationId)
+                .orElseThrow(() -> new RuntimeException("Reservation not found"));
+
+        return ReservationResponse.fromEntity(reservation);
+    }
+
+    // 예약 삭제
+    public void deleteReservation(Long reservationId) {
+        Reservation reservation = repository.findById(reservationId)
+                .orElseThrow(() -> new RuntimeException("Reservation not found"));
+
+        repository.deleteById(reservationId);
+    }
+
+    // 멤버의 예약 리스트 조회
+    public List<ReservationResponse> getReservationsByMember(Long memberId) {
+        Member member = globalService.findMemberById(memberId);
+        List<Reservation> reservations = repository.findAllByMember(member);
+
+        return reservations.stream()
+                .map(ReservationResponse::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    // 도서 예약 수 조회
+    public ReservationCountResponse getReservationCount(Long bookId) {
+        Book book = globalService.findBookById(bookId);
+
+        return ReservationCountResponse.fromEntity(repository.countByBook(book));
+    }
+}
